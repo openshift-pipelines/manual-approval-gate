e77b1a7abde13fc9acf0230feb9f0de8b903b60d
PuneetPunamiya <ppunamiy@redhat.com>
2023-11-29 14:42:46 +0530

    Adds release script for smoother git releases
    Signed-off-by: PuneetPunamiya <ppunamiy@redhat.com>

---
+++ release.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+
+RELEASE_VERSION=""
+
+DOCKER_CMD=${DOCKER_CMD:-docker}
+
+BINARIES="ko gh"
+
+info() {
+  echo "INFO: $@"
+}
+
+err() {
+  echo "ERROR: $@"
+}
+
+getReleaseVersion() {
+  [[ -z ${RELEASE_VERSION} ]] && {
+      read -r -e -p "Enter a target release (i.e: v0.1.2): " RELEASE_VERSION
+      [[ -z ${RELEASE_VERSION} ]] && {
+        echo "no target release"
+        exit 1
+      }
+    }
+    [[ ${RELEASE_VERSION} =~ v[0-9]+\.[0-9]*\.[0-9]+ ]] || {
+      echo "invalid version provided, need to match v\d+\.\d+\.\d+"
+      exit 1
+    }
+}
+
+buildImageAndGenerateReleaseYaml() {
+  	info Creating Manual Approval Gate Release Yaml for Kubernetes
+  	echo "------------------------------------------"
+    ko resolve -f config/kubernetes -t ${RELEASE_VERSION} > release-kubernetes.yaml || {
+      err 'release build failed'
+      return 1
+    }
+
+    echo "============================================="
+    info Creating Manual Approval Gate Release Yaml for Openshift
+    echo "------------------------------------------"
+    ko resolve -f config/openshift -t ${RELEASE_VERSION} > release-openshift.yaml || {
+          err 'release build failed'
+          return 1
+    }
+
+    echo "------------------------------------------"
+}
+
+createGitTag() {
+  git tag -a ${RELEASE_VERSION} -m "Manual Approval Gate '${RELEASE_VERSION}'"
+
+  git push upstream ${RELEASE_VERSION}
+}
+
+createNewPreRelease() {
+  echo; echo 'Creating New Manual Approval Gate Pre-Release :'
+
+  gh repo set-default git@github.com:openshift-pipelines/manual-approval-gate.git
+
+  gh release create --draft --prerelease -t ${RELEASE_VERSION} ${RELEASE_VERSION}
+
+  gh release upload ${RELEASE_VERSION} release-kubernetes.yaml release-openshift.yaml
+}
+
+createNewBranchAndPush() {
+  git checkout -b release-${RELEASE_VERSION}
+
+  git push upstream release-${RELEASE_VERSION}
+}
+
+main() {
+
+  # Check if all required command exists
+  for b in ${BINARIES};do
+      type -p ${b} >/dev/null || { echo "'${b}' need to be avail"; exit 1 ;}
+  done
+
+  # Ask the release version to build images
+  getReleaseVersion
+
+  # Build images for db-migration, api and ui
+  echo "********************************************"
+  info        Build the Images for Manual Approval Gate
+  echo "********************************************"
+  buildImageAndGenerateReleaseYaml
+
+  # Create and push git tag
+  echo "********************************************"
+  info        Create GIT Tag
+  echo "********************************************"
+  createGitTag
+
+  # Create a new pre-release
+  echo "********************************************"
+  info            Create New PreRelease
+  echo "********************************************"
+  createNewPreRelease
+
+  echo "********************************************"
+  info            Create New Branch And Push
+  echo "********************************************"
+  createNewBranchAndPush
+
+  echo "************************************************************"
+  echo    Release Created for Manual Approval Gate successfully
+  echo "************************************************************"
+}
+
+main $@
