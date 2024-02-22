351535b4397423ea4d1ddc9bca59afc51fb0a63c
PuneetPunamiya <ppunamiy@redhat.com>
2024-02-22 19:02:28 +0530

    Adds support for approvals from multiple users
     - With this patch now when the approvalTask is created it will look as
    
        ```
          apiVersion: openshift-pipelines.org/v1alpha1
          kind: ApprovalTask
          metadata:
            name: wait-customrun-gffjj
            namespace: default
          spec:
            approvals:
             - input: wait
               name: foo
             - input: wait
               name: bar
            - input: wait
              name: tekton
            approvalsRequired: 2
          status:
            approvalState: wait
            approvals:
             - foo
             - bar
            - tekton
        ```
    
     - User will have to patch the input field for his username
    
    - Sceanrios Handled
        - Until and unless approval task does not get approval from all the users as true,
          till that approvalState will be `wait`
        - If any one user disapproves the approval task we will mark the approvalState as
          false and then the pipelinerun will fail
        - If a user approves for the first time and still approvalsRequired limit is not
          reached i.e. approvalState is `wait` then user can still change his input and
          mark the approval task as `false`
    
    Signed-off-by: PuneetPunamiya <ppunamiy@redhat.com>

--- pkg/reconciler/approvaltask/utils_test.go
+++ pkg/reconciler/approvaltask/utils_test.go
@@ -17,10 +17,13 @@ limitations under the License.
 package approvaltask
 
 import (
+	"context"
 	"testing"
 
 	"github.com/openshift-pipelines/manual-approval-gate/pkg/apis/approvaltask"
+	"github.com/openshift-pipelines/manual-approval-gate/pkg/apis/approvaltask/v1alpha1"
 	approvaltaskv1alpha1 "github.com/openshift-pipelines/manual-approval-gate/pkg/apis/approvaltask/v1alpha1"
+	"github.com/openshift-pipelines/manual-approval-gate/pkg/client/clientset/versioned/fake"
 	"github.com/stretchr/testify/assert"
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -98,4 +101,371 @@ func TestPropagateApprovalTaskLabelsAndAnnotations(t *testing.T) {
 	expectedValue := run.Labels["openshift-pipelines.org/approvaltask"]
 	assert.Equal(t, expectedValue, "foo-bar")
 	assert.Equal(t, len(run.Labels), 1)
+}
+
+func TestCreateApprovalTask(t *testing.T) {
+	run := &v1beta1.CustomRun{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "bar",
+			Namespace: "foo",
+		},
+		Spec: v1beta1.CustomRunSpec{
+			CustomRef: &v1beta1.TaskRef{
+				APIVersion: "wrong-api-version",
+				Kind:       "wrong-kind",
+			},
+			Params: []v1beta1.Param{
+				{
+					Name:  "approvals",
+					Value: *v1beta1.NewArrayOrString("foo", "bar", "tekton"),
+				},
+				{
+					Name:  "approvalsRequired",
+					Value: *v1beta1.NewArrayOrString("2"),
+				},
+			},
+		},
+	}
+
+	client := fake.NewSimpleClientset()
+
+	approvalTask, err := createApprovalTask(context.TODO(), client, run)
+	if err != nil {
+		t.Fatalf("createApprovalTask returned an error: %v", err)
+	}
+	assert.Equal(t, nil, err)
+
+	assert.Equal(t, "bar", approvalTask.Name, "ApprovalTask name should match")
+	assert.Equal(t, "foo", approvalTask.Namespace, "ApprovalTask namespace should match")
+
+	assert.Equal(t, 3, len(approvalTask.Spec.Approvals), "Expected 3 approvals")
+	assert.Equal(t, 2, approvalTask.Spec.ApprovalsRequired, "Expected approvalsRequired to be 2")
+
+	expectedNames := []string{"foo", "bar", "tekton"}
+	for _, approval := range approvalTask.Spec.Approvals {
+		assert.Contains(t, expectedNames, approval.Name, "Approval name should be in the expected list")
+		assert.Equal(t, "wait", approval.InputValue, "Approval InputValue should be 'wait'")
+	}
+
+	assert.Equal(t, approvalTask.Status.ApprovalState, "wait", "ApprovalState should be in `wait`")
+}
+
+func TestCreateApprovalTaskWithoutApprovalsRequiredProvided(t *testing.T) {
+	run := &v1beta1.CustomRun{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "bar",
+			Namespace: "foo",
+		},
+		Spec: v1beta1.CustomRunSpec{
+			CustomRef: &v1beta1.TaskRef{
+				APIVersion: "wrong-api-version",
+				Kind:       "wrong-kind",
+			},
+			Params: []v1beta1.Param{
+				{
+					Name:  "approvals",
+					Value: *v1beta1.NewArrayOrString("foo", "bar", "tekton"),
+				},
+			},
+		},
+	}
+
+	client := fake.NewSimpleClientset()
+
+	approvalTask, err := createApprovalTask(context.TODO(), client, run)
+	if err != nil {
+		t.Fatalf("createApprovalTask returned an error: %v", err)
+	}
+	assert.Equal(t, nil, err)
+
+	assert.Equal(t, 1, approvalTask.Spec.ApprovalsRequired, "Expected approvalsRequired to be 2")
+
+	assert.Equal(t, approvalTask.Status.ApprovalState, "wait", "ApprovalState should be in `wait`")
+}
+
+func TestUpdateApprovalTaskFalseState(t *testing.T) {
+	run := &v1beta1.CustomRun{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "bar",
+			Namespace: "foo",
+		},
+		Spec: v1beta1.CustomRunSpec{
+			CustomRef: &v1beta1.TaskRef{
+				APIVersion: "wrong-api-version",
+				Kind:       "wrong-kind",
+			},
+			Params: []v1beta1.Param{
+				{
+					Name:  "approvals",
+					Value: *v1beta1.NewArrayOrString("foo", "bar", "tekton"),
+				},
+				{
+					Name:  "approvalsRequired",
+					Value: *v1beta1.NewArrayOrString("2"),
+				},
+			},
+		},
+	}
+
+	client := fake.NewSimpleClientset()
+
+	approvalTask, err := createApprovalTask(context.TODO(), client, run)
+	if err != nil {
+		t.Fatalf("createApprovalTask returned an error: %v", err)
+	}
+	assert.Equal(t, nil, err)
+	assert.Equal(t, approvalTask.Status.ApprovalState, "wait", "ApprovalState should be in `wait`")
+
+	approvalTask.Spec.Approvals[0].InputValue = "false"
+
+	at, err := client.OpenshiftpipelinesV1alpha1().ApprovalTasks(approvalTask.Namespace).Update(context.TODO(), &approvalTask, metav1.UpdateOptions{})
+	if err != nil {
+		t.Fatalf("failed updating the input value for for")
+	}
+
+	at1, err := updateApprovalState(context.TODO(), client, at)
+	if err != nil {
+		t.Fatalf("updateApprovalTask returned an error: %v", err)
+	}
+	assert.Equal(t, nil, err)
+
+	assert.Equal(t, at1.Status.ApprovalState, "false", "ApprovalState should be in `false`")
+	assert.Equal(t, len(at1.Status.ApprovedBy), 1, "foo has approved it")
+}
+
+func TestUpdateApprovalTaskWaitState(t *testing.T) {
+	run := &v1beta1.CustomRun{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "bar",
+			Namespace: "foo",
+		},
+		Spec: v1beta1.CustomRunSpec{
+			CustomRef: &v1beta1.TaskRef{
+				APIVersion: "wrong-api-version",
+				Kind:       "wrong-kind",
+			},
+			Params: []v1beta1.Param{
+				{
+					Name:  "approvals",
+					Value: *v1beta1.NewArrayOrString("foo", "bar", "tekton"),
+				},
+				{
+					Name:  "approvalsRequired",
+					Value: *v1beta1.NewArrayOrString("2"),
+				},
+			},
+		},
+	}
+
+	client := fake.NewSimpleClientset()
+
+	approvalTask, err := createApprovalTask(context.TODO(), client, run)
+	if err != nil {
+		t.Fatalf("createApprovalTask returned an error: %v", err)
+	}
+	assert.Equal(t, nil, err)
+	assert.Equal(t, approvalTask.Status.ApprovalState, "wait", "ApprovalState should be in `wait`")
+
+	approvalTask.Spec.Approvals[0].InputValue = "true"
+	at, err := client.OpenshiftpipelinesV1alpha1().ApprovalTasks(approvalTask.Namespace).Update(context.TODO(), &approvalTask, metav1.UpdateOptions{})
+	if err != nil {
+		t.Fatalf("failed updating the input value for for")
+	}
+
+	at1, err := updateApprovalState(context.TODO(), client, at)
+	if err != nil {
+		t.Fatalf("updateApprovalTask returned an error: %v", err)
+	}
+	assert.Equal(t, nil, err)
+
+	assert.Equal(t, at1.Status.ApprovalState, "wait", "ApprovalState should be in `wait`")
+	assert.Equal(t, len(at1.Status.ApprovedBy), 1, "foo has approved it")
+	assert.Equal(t, at1.Status.ApprovedBy[0].Name, "foo", "foo has approved it")
+	assert.Equal(t, at1.Status.ApprovedBy[0].Approved, "true", "foo has approved it")
+}
+
+func TestUpdateApprovalTaskTrueState(t *testing.T) {
+	run := &v1beta1.CustomRun{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "bar",
+			Namespace: "foo",
+		},
+		Spec: v1beta1.CustomRunSpec{
+			CustomRef: &v1beta1.TaskRef{
+				APIVersion: "wrong-api-version",
+				Kind:       "wrong-kind",
+			},
+			Params: []v1beta1.Param{
+				{
+					Name:  "approvals",
+					Value: *v1beta1.NewArrayOrString("foo", "bar", "tekton"),
+				},
+				{
+					Name:  "approvalsRequired",
+					Value: *v1beta1.NewArrayOrString("2"),
+				},
+			},
+		},
+	}
+
+	client := fake.NewSimpleClientset()
+
+	approvalTask, err := createApprovalTask(context.TODO(), client, run)
+	if err != nil {
+		t.Fatalf("createApprovalTask returned an error: %v", err)
+	}
+	assert.Equal(t, nil, err)
+	assert.Equal(t, approvalTask.Status.ApprovalState, "wait", "ApprovalState should be in `wait`")
+
+	approvalTask.Spec.Approvals[0].InputValue = "true"
+	approvalTask.Spec.Approvals[1].InputValue = "true"
+
+	at, err := client.OpenshiftpipelinesV1alpha1().ApprovalTasks(approvalTask.Namespace).Update(context.TODO(), &approvalTask, metav1.UpdateOptions{})
+	if err != nil {
+		t.Fatalf("failed updating the input value for for")
+	}
+
+	at1, err := updateApprovalState(context.TODO(), client, at)
+	if err != nil {
+		t.Fatalf("updateApprovalTask returned an error: %v", err)
+	}
+	assert.Equal(t, nil, err)
+
+	assert.Equal(t, at1.Status.ApprovalState, "true", "ApprovalState should be in `true`")
+	assert.Equal(t, len(at1.Status.ApprovedBy), 2, "foo has approved it")
+}
+
+func TestUpdateApprovalTaskWithNoApprovalsProvided(t *testing.T) {
+	run := &v1beta1.CustomRun{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "bar",
+			Namespace: "foo",
+		},
+		Spec: v1beta1.CustomRunSpec{
+			CustomRef: &v1beta1.TaskRef{
+				APIVersion: "wrong-api-version",
+				Kind:       "wrong-kind",
+			},
+		},
+	}
+
+	client := fake.NewSimpleClientset()
+
+	approvalTask, err := createApprovalTask(context.TODO(), client, run)
+	if err != nil {
+		t.Fatalf("createApprovalTask returned an error: %v", err)
+	}
+	assert.Equal(t, nil, err)
+	assert.Equal(t, approvalTask.Status.ApprovalState, "wait", "ApprovalState should be in `wait`")
+	assert.Equal(t, approvalTask.Spec.ApprovalsRequired, 1, "ApprovalsRequired should be 1`")
+
+	approvals := v1alpha1.Input{
+		Name:       "foo",
+		InputValue: "true",
+	}
+	approvalTask.Spec.Approvals = append(approvalTask.Spec.Approvals, approvals)
+
+	at, err := client.OpenshiftpipelinesV1alpha1().ApprovalTasks(approvalTask.Namespace).Update(context.TODO(), &approvalTask, metav1.UpdateOptions{})
+	if err != nil {
+		t.Fatalf("failed updating the input value for for")
+	}
+
+	at1, err := updateApprovalState(context.TODO(), client, at)
+	if err != nil {
+		t.Fatalf("updateApprovalTask returned an error: %v", err)
+	}
+	assert.Equal(t, nil, err)
+
+	assert.Equal(t, at1.Status.ApprovalState, "true", "ApprovalState should be in `true`")
+	assert.Equal(t, len(at1.Status.ApprovedBy), 1, "foo has approved it")
+}
+
+func TestApprovalTaskHasFalseInputWithOneApproval(t *testing.T) {
+	approvaltask := v1alpha1.ApprovalTask{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "bar",
+		},
+		Spec: v1alpha1.ApprovalTaskSpec{
+			Approvals: []v1alpha1.Input{
+				{
+					Name:       "apple",
+					InputValue: "false",
+				},
+				{
+					Name:       "banana",
+					InputValue: "wait",
+				},
+				{
+					Name:       "mango",
+					InputValue: "true",
+				},
+			},
+			ApprovalsRequired: 2,
+		},
+		Status: v1alpha1.ApprovalTaskStatus{},
+	}
+
+	got := approvalTaskHasFalseInput(approvaltask)
+	assert.Equal(t, true, got)
+}
+
+func TestApprovalTaskHasTrueInputWithAllApprovals(t *testing.T) {
+	approvaltask := v1alpha1.ApprovalTask{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "bar",
+		},
+		Spec: v1alpha1.ApprovalTaskSpec{
+			Approvals: []v1alpha1.Input{
+				{
+					Name:       "foo",
+					InputValue: "wait",
+				},
+				{
+					Name:       "bar",
+					InputValue: "true",
+				},
+				{
+					Name:       "tekton",
+					InputValue: "true",
+				},
+			},
+			ApprovalsRequired: 2,
+		},
+		Status: v1alpha1.ApprovalTaskStatus{},
+	}
+
+	got := approvalTaskHasTrueInput(approvaltask)
+	assert.Equal(t, true, got)
+}
+
+func TestApprovalTaskHasTrueInputWithSomeApprovals(t *testing.T) {
+	approvaltask := v1alpha1.ApprovalTask{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "bar",
+		},
+		Spec: v1alpha1.ApprovalTaskSpec{
+			Approvals: []v1alpha1.Input{
+				{
+					Name:       "foo",
+					InputValue: "wait",
+				},
+				{
+					Name:       "bar",
+					InputValue: "wait",
+				},
+				{
+					Name:       "tekton",
+					InputValue: "true",
+				},
+			},
+			ApprovalsRequired: 2,
+		},
+		Status: v1alpha1.ApprovalTaskStatus{},
+	}
+
+	got := approvalTaskHasTrueInput(approvaltask)
+	assert.Equal(t, false, got)
 }
